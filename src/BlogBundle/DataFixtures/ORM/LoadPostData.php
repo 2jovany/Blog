520b9c5bdec703bca3fc201097cf78e2a5b6aca6
Jovany <jovany@ua.fm>
2017-01-16 05:10:56 +0200

    Generate more categories and posts

--- src/BlogBundle/DataFixtures/ORM/LoadPostData.php
+++ src/BlogBundle/DataFixtures/ORM/LoadPostData.php
@@ -11,24 +11,30 @@ class LoadPostData implements FixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        $category = new Category();
-        $theTimeIs = new \DateTime('now');
-        $category->setTitle('Test category name')
-            ->setImage('<img src="/apple-touch-icon.png" >')
-            ->setContent('Test Category Description')
-            ->setCreatedAt($theTimeIs);
-        $manager->persist($category);
-        $manager->flush();
+        $num = 0;
+        for ($i = 1; $i <= 3; $i++) {
+            $category = new Category();
+            $theTimeIs = new \DateTime('now');
+            $category->setTitle("Test category name - $i")
+                ->setImage('<img src="/apple-touch-icon.png" >')
+                ->setContent("Test Category Description - $i")
+                ->setCreatedAt($theTimeIs);
+            $manager->persist($category);
+            $manager->flush();
 
-        $post = new Post();
-        $theTimeIs = new \DateTime("now");
-        $category_id = $category->getId();
-        $post->setCategoryId($category_id)
-            ->setTitle('Post title')
-            ->setAuthor('Jovany')
-            ->setContent('Blah Blah Blah...')
-            ->setCreatedAt($theTimeIs);
-        $manager->persist($post);
-        $manager->flush();
+            for ($y = 1; $y <= 5; $y++) {
+                $num++;
+                $post = new Post();
+                $theTimeIs = new \DateTime("now");
+                $category_id = $category->getId();
+                $post->setCategoryId($category_id)
+                    ->setTitle("Post title - $num")
+                    ->setAuthor('Jovany')
+                    ->setContent("Blah Blah Blah... - $num")
+                    ->setCreatedAt($theTimeIs);
+                $manager->persist($post);
+                $manager->flush();
+            }
+        }
     }
 }
